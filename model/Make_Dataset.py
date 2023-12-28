1e6d7266dce227bc935e325c00149c8853395bea
JeongEul Kwon <wjddmf0321@naver.com>
2023-12-28 16:08:33 +0900

    Code

--- model/Make_Dataset.py
+++ model/Make_Dataset.py
@@ -4,58 +4,50 @@ import random
 from tqdm import tqdm
 from sklearn.preprocessing import LabelEncoder
 
+path = '/Users/DAHS/Desktop/MIMICIV2.2_PREPROC/data/csv/'
+cohort = '/Users/DAHS/Desktop/MIMICIV2.2_PREPROC/data/demo.csv'
+
 class Integration_data():
-    def __init__(self, get_data, first_try):
-        self.categorical_encoding(get_data, first_try)
+    def __init__(self):
+        self.categorical_encoding()
+        
     def create_stay_id(self):
-        data=pd.read_csv('/Users/DAHS/Desktop/circ_mimic_preprocessing_1day/data/demo.csv', index_col = 0)
+        data=pd.read_csv(cohort, index_col = 0)
 
         hids=data['stay_id'].unique()
         print("Total stay",len(hids))
-        return hids
+        return data, hids
     
-    def categorical_encoding(self, get_data, first_try):
-        hids=self.create_stay_id()
-        
-        if (get_data == True)&(first_try == True):
-            data=self.getdata(hids)
-            
-        else:
-            data = pd.read_csv('Total.csv')
-        
+    def categorical_encoding(self):
+        data, hids=self.create_stay_id()
+        data=self.getdata(data, hids)
         return data
         
             
-    def getdata(self,ids):
+    def getdata(self, dataset, ids):
         df_list = []   
-        data=pd.read_csv('/Users/DAHS/Desktop/circ_mimic_preprocessing_1day/data/demo.csv', index_col = 0)
         for sample in tqdm(ids):
-            dyn=pd.read_csv('/Users/DAHS/Desktop/circ_mimic_preprocessing_1day/Data/csv/'+str(sample)+'.0/dynamic.csv',header=[0,1])
-            stat = data[data['stay_id']==int(sample)]
+            dyn=pd.read_csv(path+str(sample)+'.0/dynamic_proc.csv',header=[0,1])
+            stat = dataset[dataset['stay_id']==int(sample)]
             
-            dyn.columns=dyn.columns.droplevel(0)
-            columns_to_copy = ['subject_id', 'stay_id', 'hadm_id', 'Age', 'gender', 'ethnicity', 'insurance']
+            columns_to_copy = ['subject_id', 'stay_id', 'hadm_id', 'Age', 'gender', 'ethnicity']
             for column in columns_to_copy:
-                dyn[column] = stat[column].values[0]
-                
+                dyn[(  'demographic',        column)] = stat[column].values[0] 
             df_list.append(dyn)
-            
+  
         df = pd.concat(df_list, axis = 0)
         
         print("total stay dataframe shape",df.shape)
-        
+        df.columns=df.columns.droplevel(0)
         #encoding categorical
         gen_encoder = LabelEncoder()
         eth_encoder = LabelEncoder()
-        ins_encoder = LabelEncoder()
 
         gen_encoder.fit(df['gender'])
         eth_encoder.fit(df['ethnicity'])
-        ins_encoder.fit(df['insurance'])
-
+    
         df['gender']=gen_encoder.transform(df['gender'])
         df['ethnicity']=eth_encoder.transform(df['ethnicity'])
-        df['insurance']=ins_encoder.transform(df['insurance'])
         
-        df.to_csv('Total.csv')
+        df.to_csv('MIMICIV2.0_semi_preproc.csv')
         return df
